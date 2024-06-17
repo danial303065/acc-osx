c06df88fda9e975a28e388f9ce8a13d64247d4bd
Danial <win303065@gmail.com>
2024-06-17 17:12:58 +0900

    Remove faker

--- packages/faker/docker-build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-TAG_NANE="$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short=6 HEAD)"
-echo "TAG_NANE=$TAG_NANE"
-
-docker build -t bosagora/dms-faker:"$TAG_NANE" -f Dockerfile --push .
